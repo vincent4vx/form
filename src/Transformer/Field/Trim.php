abe7a5b8619f1096b8f67bc2f8ce4b8dc0635672
Vincent Quatrevieux <quatrevieux.vincent@gmail.com>
2023-03-09 17:33:49 +0100

    feat: Add trim transformer

---
+++ src/Transformer/Field/Trim.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace Quatrevieux\Form\Transformer\Field;
+
+use Attribute;
+use Quatrevieux\Form\Transformer\Generator\FieldTransformerGeneratorInterface;
+use Quatrevieux\Form\Transformer\Generator\FormTransformerGenerator;
+
+use Quatrevieux\Form\Util\Code;
+use Quatrevieux\Form\Util\Expr;
+
+use function is_scalar;
+use function trim;
+
+/**
+ * Trim the field value
+ *
+ * This transformer will remove all spaces at the beginning and at the end of the value.
+ * The transformation is only applied when transforming from HTTP.
+ *
+ * Usage:
+ * <code>
+ * class MyForm
+ * {
+ *     #[Trim]
+ *     public string $name;
+ * }
+ * </code>
+ *
+ * @implements FieldTransformerGeneratorInterface<Trim>
+ */
+#[Attribute(Attribute::TARGET_PROPERTY)]
+final class Trim implements FieldTransformerInterface, FieldTransformerGeneratorInterface
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function transformFromHttp(mixed $value): ?string
+    {
+        if (!is_scalar($value)) {
+            return null;
+        }
+
+        return trim((string) $value);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function transformToHttp(mixed $value): mixed
+    {
+        return $value;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function canThrowError(): bool
+    {
+        return false;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function generateTransformFromHttp(object $transformer, string $previousExpression, FormTransformerGenerator $generator): string
+    {
+        return Code::expr($previousExpression)->storeAndFormat('is_scalar({}) ? trim((string) {}) : null');
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function generateTransformToHttp(object $transformer, string $previousExpression, FormTransformerGenerator $generator): string
+    {
+        return $previousExpression;
+    }
+}
---
+++ tests/Transformer/Field/TrimTest.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace Quatrevieux\Form\Transformer\Field;
+
+use Quatrevieux\Form\FormTestCase;
+use Quatrevieux\Form\Transformer\Generator\FormTransformerGenerator;
+
+class TrimTest extends FormTestCase
+{
+    /**
+     * @testWith [true]
+     *           [false]
+     */
+    public function test_from_http(bool $generated)
+    {
+        $form = $generated ? $this->generatedForm(TrimTestRequest::class) : $this->runtimeForm(TrimTestRequest::class);
+
+        $this->assertNull($form->submit([])->value()->foo);
+        $this->assertSame('a', $form->submit(['foo' => ' a  '])->value()->foo);
+        $this->assertSame('a', $form->submit(['foo' => 'a'])->value()->foo);
+        $this->assertSame('12.3', $form->submit(['foo' => 12.3])->value()->foo);
+    }
+
+    /**
+     * @testWith [true]
+     *           [false]
+     */
+    public function test_to_http_should_do_nothing(bool $generated)
+    {
+        $form = $generated ? $this->generatedForm(TrimTestRequest::class) : $this->runtimeForm(TrimTestRequest::class);
+
+        $data = new TrimTestRequest();
+        $data->foo = 'a ';
+
+        $imported = $form->import($data);
+
+        $this->assertSame(['foo' => 'a '], $imported->httpValue());
+    }
+
+    public function test_generate_transform_from_http()
+    {
+        $transformer = new Trim();
+        $generator = new FormTransformerGenerator($this->registry);
+
+        $this->assertSame('is_scalar(($__tmp_cf8d20da9cb97be602abb1ce003a22b3 = $data["foo"] ?? null)) ? trim((string) $__tmp_cf8d20da9cb97be602abb1ce003a22b3) : null', $transformer->generateTransformFromHttp($transformer, '$data["foo"] ?? null', $generator));
+    }
+
+    public function test_generate_transform_to_http()
+    {
+        $transformer = new Trim();
+        $generator = new FormTransformerGenerator($this->registry);
+
+        $this->assertSame('$data["foo"] ?? null', $transformer->generateTransformToHttp($transformer, '$data["foo"] ?? null', $generator));
+    }
+}
+
+class TrimTestRequest
+{
+    #[Trim]
+    public ?string $foo;
+}
